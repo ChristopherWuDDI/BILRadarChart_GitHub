429cc1db6d6d45dc065e4919040d44e4677a2463
ChristopherWu <dingbin.wu@hmit.com.cn>
2015-08-14 13:10:51 +0800

    testing

---
+++ RadarChart.Application.Services/IRoleService.cs
@@ -0,0 +1,22 @@
+﻿using RadarChart.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarChart.Application.Services
+{
+    public interface IRoleService
+    {
+        void Add(Role r);
+
+        List<Role> GetAll();
+
+        Role GetOne(string roleId);
+
+        void Modify(Role r);
+
+        void Delete(Role r);
+    }
+}
---
+++ RadarChart.Application.Services/Impl/RoleService.cs
@@ -0,0 +1,40 @@
+﻿using RadarChart.Core.Model;
+using RadarChart.Core.Repository.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarChart.Application.Services.Impl
+{
+    public class RoleService : IRoleService
+    {
+        RoleRepository rr = new RoleRepository();
+
+        public void Add(Role r)
+        {
+            rr.Add(r);
+        }
+
+        public List<Role> GetAll()
+        {
+            return rr.GetAll();
+        }
+
+        public Role GetOne(string roleId)
+        {
+            return rr.GetOne(arole => arole.RoleId == roleId);
+        }
+
+        public void Modify(Role r)
+        {
+            rr.Modify(r);
+        }
+
+        public void Delete(Role r)
+        {
+            rr.Delete(r);
+        }
+    }
+}
